8f03edd88a631296600d893853cec1003b6eca80
Leanne Werner <leanne.werner.lw@gmail.com>
2022-04-12 22:40:04 -0700

    control point functionality wip

--- components/PenTool/Path.tsx
+++ components/PenTool/Path.tsx
@@ -1,7 +1,16 @@
 import { Line } from "@react-three/drei";
-import { useCallback } from "react";
+import { useThree } from "@react-three/fiber";
+import { useCallback, useEffect, useState } from "react";
 import * as THREE from "three";
+import { getRealMouseCoordinates } from "./utils";
 import { VectorNode } from "./VectorNode";
+
+type ControlPointObj = {
+  curve: THREE.Curve<THREE.Vector2>;
+  firstControlPoint: THREE.Vector3[];
+  secondControlPoint: THREE.Vector3[];
+  index: number;
+};
 
 export function Path({
   points,
@@ -11,8 +20,14 @@ export function Path({
   isClosed: boolean;
 }) {
   const path = new THREE.Path();
+  const [isMoving, setIsMoving] = useState(false);
+  // const [controlPointObjs, setControlPoints] =
+  //   useState<ControlPointObj[]>(null);
+  const [pointIndexToMove, setPointIndexToMove] = useState<number | null>(null);
+  const { camera } = useThree();
 
   path.moveTo(points[0].x, points[0].y);
+
   points.forEach((point) => {
     path.bezierCurveTo(
       point.x + 1,
@@ -25,17 +40,96 @@ export function Path({
   });
 
   const pathPoints = path.getPoints();
-  // console.log({ path });
+
+  const controlPointObjs: ControlPointObj[] = path.curves.map(
+    (curve, index) => {
+      const firstControlPoint = [
+        new THREE.Vector3(curve["v0"].x, curve["v0"].y, 0),
+        new THREE.Vector3(curve["v1"].x, curve["v1"].y, 0),
+      ];
+
+      const secondControlPoint = [
+        new THREE.Vector3(curve["v2"].x, curve["v2"].y, 0),
+        new THREE.Vector3(curve["v3"].x, curve["v3"].y, 0),
+      ];
+      return { curve, firstControlPoint, secondControlPoint, index };
+    }
+  );
+
+  console.log(controlPointObjs);
+
   const onUpdatePath = useCallback(
     (self) => self.setFromPoints(pathPoints),
     [pathPoints]
   );
 
+  function handlePointerDown(e: PointerEvent) {
+    e.preventDefault();
+    const mouseCoordinates = getRealMouseCoordinates(e, camera);
+    const newPoint = new THREE.Vector3(
+      mouseCoordinates.x,
+      mouseCoordinates.y,
+      0
+    );
+
+    // controlPointObjs.forEach((point, index) => {
+    //   if (point.distanceTo(newPoint) < 0.051) {
+    //     setIsMoving(true);
+    //     setPointIndexToMove(index);
+    //     return;
+    //   }
+    // });
+
+    // if (
+    //   controlPointObjs.filter((point) => point.distanceTo(newPoint) < 0.051)
+    //     .length === 0
+    // ) {
+    //   setControlPoints([...controlPointObjs, newPoint]);
+    // }
+  }
+
+  function handlePointerUp(e: PointerEvent) {
+    e.preventDefault();
+    setIsMoving(false);
+    setPointIndexToMove(null);
+  }
+
+  function handlePointerMove(e: PointerEvent) {
+    e.preventDefault();
+    if (isMoving) {
+      const mouseCoordinates = getRealMouseCoordinates(e, camera);
+      const newPoint = new THREE.Vector3(
+        mouseCoordinates.x,
+        mouseCoordinates.y,
+        0
+      );
+      updateVectorNode(newPoint, pointIndexToMove);
+    }
+  }
+
+  function updateVectorNode(point: THREE.Vector3, index: number) {
+    // setControlPoints([
+    //   ...points.slice(0, index),
+    //   point,
+    //   ...points.slice(index + 1),
+    // ]);
+  }
+
+  useEffect(() => {
+    window.addEventListener("pointerdown", handlePointerDown);
+    window.addEventListener("pointerup", handlePointerUp);
+    window.addEventListener("pointermove", handlePointerMove);
+    return () => {
+      window.removeEventListener("pointerdown", handlePointerDown);
+      window.removeEventListener("pointerup", handlePointerUp);
+      window.removeEventListener("pointermove", handlePointerMove);
+    };
+  }, [controlPointObjs, isMoving]);
+
   return (
     <>
       {path.curves.map((curve, index) => {
         // Create curve handels based off control points https://threejs.org/docs/?q=curve#api/en/extras/curves/CubicBezierCurve
-
         let firstControlPoint = [
           new THREE.Vector3(curve["v0"].x, curve["v0"].y, 0),
           new THREE.Vector3(curve["v1"].x, curve["v1"].y, 0),
@@ -49,19 +143,27 @@ export function Path({
         return (
           <>
             <Line
-              key={index}
+              key={index + 1}
               points={firstControlPoint}
               color="blue"
               lineWidth={1}
             />
-            <VectorNode point={firstControlPoint[1]} size={0.04} />
+            <VectorNode
+              key={index + 2}
+              point={firstControlPoint[1]}
+              size={0.04}
+            />
             <Line
-              key={index}
+              key={index + 3}
               points={secondControlPoint}
               color="red"
               lineWidth={1}
             />
-            <VectorNode point={secondControlPoint[0]} size={0.04} />
+            <VectorNode
+              key={index + 4}
+              point={secondControlPoint[0]}
+              size={0.04}
+            />
           </>
         );
       })}
